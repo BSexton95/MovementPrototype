59eb2aef1fb0c6ce4332fba025c5b73c638eb766
Brittnie Sexton <brittniesexton95@gmail.com>
2023-02-02 12:50:04 -0600

    Recreated first person moving. Starting on camera animation

---
+++ Source/MovementPrototype/MovementPrototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MovementPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MovementPrototype, "MovementPrototype" );
